11fb408a5ee06d03b0999a44e81eae4dc571c611
suryaguntursuprapto <suryaguntur2000@gmail.com>
2025-01-20 11:53:36 +0700

    membuat model Event

---
+++ app/src/main/java/com/example/tugasakhirpam/model/Event.kt
@@ -0,0 +1,24 @@
+package com.example.tugasakhirpam.model
+
+
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
+import kotlinx.serialization.descriptors.*
+import kotlinx.serialization.encoding.*
+
+
+@Serializable
+data class Event (
+    @SerialName("id_event")
+    val idEvent: String,
+
+    @SerialName("nama_event")
+    val namaEvent: String,
+
+    @SerialName("deskripsi_event")
+    val deskripsiEvent: String,
+
+    @SerialName("tanggal_event")
+    val tanggalEvent: String
+)
+
