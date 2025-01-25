32a52d89bfb0c33bc0ef37f8c44ef97f347c95f8
suryaguntursuprapto <suryaguntur2000@gmail.com>
2025-01-25 19:23:53 +0700

    membuat resource network_security_config.xml dan menambahkannya ke AndroidManifest.xml

--- app/src/main/java/com/example/tugasakhirpam/MainActivity.kt
+++ app/src/main/java/com/example/tugasakhirpam/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.tugasakhirpam.ui.theme.TugasAkhirPAMTheme
+import com.example.tugasakhirpam.ui.viewmodel.KonserApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,12 +20,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TugasAkhirPAMTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+               KonserApp()
             }
         }
     }
