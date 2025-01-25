c1936f37abd5e5e13c08b26492d6bad03d688a43
suryaguntursuprapto <suryaguntur2000@gmail.com>
2025-01-25 19:26:42 +0700

    memperbarui DestinasiNavigasi

--- app/src/main/java/com/example/tugasakhirpam/navigation/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/tugasakhirpam/navigation/DestinasiNavigasi.kt
@@ -10,3 +10,14 @@ object DestinasiHomePeserta : DestinasiNavigasi {
     override val route = "home_peserta"
     override val titleRes = "Home Peserta"
 }
+
+object DestinasiInsertPeserta : DestinasiNavigasi {
+    override val route = "Insert_peserta"
+    override val titleRes = "Insert Peserta"
+}
+
+object DestinasiDetailPeserta : DestinasiNavigasi {
+    override val route = "detail_peserta/{id_peserta}"
+    override val titleRes = "Detail Peserta"
+    const val idArg = "id"
+}
