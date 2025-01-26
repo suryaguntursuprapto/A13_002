65815301baa858b9a7600ec7700dfedb62021f54
suryaguntursuprapto <suryaguntur2000@gmail.com>
2025-01-26 09:10:33 +0700

    memperbaiki DestinasiNavigasi

--- app/src/main/java/com/example/tugasakhirpam/navigation/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/tugasakhirpam/navigation/DestinasiNavigasi.kt
@@ -5,6 +5,11 @@ interface DestinasiNavigasi {
     val titleRes: String
 }
 
+object DestinasiHalamanAwalKonser : DestinasiNavigasi {
+    override val route = "halaman_awal_konser"
+    override val titleRes = "Halaman Awal Konser"
+}
+
 
 object DestinasiHomePeserta : DestinasiNavigasi {
     override val route = "home_peserta"
@@ -19,5 +24,21 @@ object DestinasiInsertPeserta : DestinasiNavigasi {
 object DestinasiDetailPeserta : DestinasiNavigasi {
     override val route = "detail_peserta/{id_peserta}"
     override val titleRes = "Detail Peserta"
-    const val idArg = "id"
+    const val idArg = "id_peserta"
+}
+
+object DestinasiEditPeserta : DestinasiNavigasi {
+    override val route = "edit_peserta/{id_peserta}"
+    override val titleRes = "Edit Peserta"
+    const val idArg = "id_peserta"
+
+    fun createRoute(idPeserta: String): String {
+        return "edit_peserta/$idPeserta"
+    }
+}
+
+//EVENT
+object DestinasiHomeEvent : DestinasiNavigasi {
+    override val route = "home_event"
+    override val titleRes = "Home Event"
 }
