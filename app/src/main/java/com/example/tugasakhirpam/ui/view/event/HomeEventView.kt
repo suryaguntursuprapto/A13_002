217a1e452049ac0ba54758c6251060b3fe6c7400
suryaguntursuprapto <suryaguntur2000@gmail.com>
2025-01-26 09:09:57 +0700

    membuat HomeEventView

---
+++ app/src/main/java/com/example/tugasakhirpam/ui/view/event/HomeEventView.kt
@@ -0,0 +1,221 @@
+package com.example.tugasakhirpam.ui.view.event
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.tugasakhirpam.R
+import com.example.tugasakhirpam.model.Event
+import com.example.tugasakhirpam.ui.customwidget.CostumeTopAppBar
+import com.example.tugasakhirpam.ui.viewmodel.event.HomeEventUiState
+import com.example.tugasakhirpam.ui.viewmodel.event.HomeEventViewModel
+import com.example.tugasakhirpam.ui.viewmodel.PenyediaViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeEventScreen(
+    navigateBack: () -> Unit,
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (String) -> Unit = {},
+    viewModel: HomeEventViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = "Events", // You can update this with the appropriate string resource
+                canNavigateBack = true,
+                navigateUp = navigateBack,
+                scrollBehavior = scrollBehavior,
+                onRefresh = {
+                    viewModel.getEvent()
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Add Event")
+            }
+        }
+    ) { innerPadding ->
+        HomeEventStatus(
+            homeUiState = viewModel.eventUiState,
+            retryAction = { viewModel.getEvent() },
+            modifier = Modifier.padding(innerPadding),
+            onDetailClick = onDetailClick,
+            onDeleteClick = {
+                viewModel.deleteEvent(it.idEvent)
+                viewModel.getEvent()
+            }
+        )
+    }
+}
+
+@Composable
+fun HomeEventStatus(
+    homeUiState: HomeEventUiState,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Event) -> Unit = {},
+    onDetailClick: (String) -> Unit
+) {
+    when (homeUiState) {
+        is HomeEventUiState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
+
+        is HomeEventUiState.Success -> {
+            if (homeUiState.events.isEmpty()) {
+                Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Text(text = "Tidak ada data Event")
+                }
+            } else {
+                EventLayout(
+                    events = homeUiState.events,
+                    modifier = modifier.fillMaxWidth(),
+                    onDetailClick = { onDetailClick(it.idEvent) },
+                    onDeleteClick = { onDeleteClick(it) }
+                )
+            }
+        }
+
+        is HomeEventUiState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
+    }
+}
+
+@Composable
+fun OnLoading(modifier: Modifier = Modifier){
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.loading),
+        contentDescription = stringResource(R.string.loading)
+    )
+}
+
+@Composable
+fun OnError(retryAction: () -> Unit,modifier: Modifier = Modifier){
+    Column (
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.connectioneror,), contentDescription = ""
+        )
+        Text(text = stringResource(R.string.loading_failed), modifier = Modifier.padding(16.dp))
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
+        }
+    }
+}
+
+@Composable
+fun EventLayout(
+    events: List<Event>,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Event) -> Unit,
+    onDeleteClick: (Event) -> Unit = {}
+) {
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(events) { item ->
+            EventCard(
+                event = item,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { onDetailClick(item) },
+                onDeleteClick = {
+                    onDeleteClick(item)
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun EventCard(
+    event: Event,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Event) -> Unit = {}
+) {
+    Card(
+        modifier = modifier,
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = event.namaEvent,
+                    style = MaterialTheme.typography.titleLarge,
+                )
+                Spacer(Modifier.weight(1f))
+                IconButton(onClick = { onDeleteClick(event) }) {
+                    Icon(
+                        imageVector = Icons.Default.Delete,
+                        contentDescription = null,
+                    )
+                }
+                Text(
+                    text = event.idEvent,
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
+
+            Text(
+                text = event.tanggalEvent,
+                style = MaterialTheme.typography.titleMedium
+            )
+            Text(
+                text = event.deskripsiEvent,
+                style = MaterialTheme.typography.titleMedium
+            )
+        }
+    }
+}
